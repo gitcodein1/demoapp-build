25ea2d48f0a93e5852c7dba27f604eb51bce3557
gitcodein1 <145888378+gitcodein1@users.noreply.github.com>
2024-09-16 14:52:01 +0530

    Update image_build.sh

--- image_build.sh
+++ image_build.sh
@@ -9,11 +9,19 @@ base_image="tomcat:alpine"
 if docker inspect $base_image &>/dev/null
 then
         echo "$base_image exists"
+        echo "No need to Pull..."
         docker build --build-arg image_name=$base_image -t demoapp .
+        echo
+        echo "Build Finished..."
 else
+        echo "$base_image Not Available on the Host"
         echo $secret_PSW | docker login -u $secret_USR --password-stdin $proxy_repo
+        echo "Pull the image from https://$proxy_repo"
         docker pull $proxy_repo/$base_image
         docker tag $proxy_repo/$base_image $base_image
         docker logout
+        echo
         docker build --build-arg image_name=$base_image -t demoapp .
+        echo
+        echo "Build Finished..."
 fi
