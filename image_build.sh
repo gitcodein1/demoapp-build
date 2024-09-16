9f7f13b2a6aa229ff84ff11b6707f40d2c9a8bb3
gitcodein1 <145888378+gitcodein1@users.noreply.github.com>
2024-09-16 14:16:50 +0530

    Update image_build.sh

--- image_build.sh
+++ image_build.sh
@@ -2,7 +2,7 @@
 
 # condition returns 0 execute if block, otherwise execute else block
 
-proxy_repo="e5e6-183-87-250-107.ngrok-free.app"
+proxy_repo="b156-183-87-250-107.ngrok-free.app"
 
 base_image="tomcat:alpine"
 
