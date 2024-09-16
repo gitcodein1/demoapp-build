1efd61f516969f23de52f23158bdbed8c43fbd46
gitcodein1 <145888378+gitcodein1@users.noreply.github.com>
2024-09-16 14:48:56 +0530

    Update image_build.sh

--- image_build.sh
+++ image_build.sh
@@ -2,18 +2,18 @@
 
 # condition returns 0 execute if block, otherwise execute else block
 
-proxy_repo="e5e6-183-87-250-107.ngrok-free.app"
+proxy_repo=""
 
 base_image="tomcat:alpine"
 
-if docker inspect $image_name &>/dev/null
+if docker inspect $base_image &>/dev/null
 then
         echo "$base_image exists"
-        docker build -t demoapp .
+        docker build --build-arg image_name=$base_image -t demoapp .
 else
         echo $secret_PSW | docker login -u $secret_USR --password-stdin $proxy_repo
         docker pull $proxy_repo/$base_image
         docker tag $proxy_repo/$base_image $base_image
         docker logout
-        docker build -t demoapp .
+        docker build --build-arg image_name=$base_image -t demoapp .
 fi
