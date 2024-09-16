98b072462ee34d89a5a5504ab09d368d53005cff
gitcodein1 <145888378+gitcodein1@users.noreply.github.com>
2024-09-16 13:50:31 +0530

    Update artifact_pull.sh

--- artifact_pull.sh
+++ artifact_pull.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-repo_url="localhost:1081/repository/demo-maven-hosted-snapshot"
-group="com/demo"
-artifactid="demo-app"
-version="1.0.0-SNAPSHOT"
-filename="demo-app-1.0.0-20240915.205244-1.war"
+echo ">>Gathering Artifact Info<<"
+echo "---------------------------"
+source artifact_info
+echo
+echo ">>Downloading Artifact<<"
+echo "------------------------"
 curl -u $secret_USR:$secret_PSW -o demoapp.war http://$repo_url/$group/$artifactid/$version/$filename
+echo "**Finished**"
+echo
